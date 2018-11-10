bac8f9303e3d5fb6e9810661ee7990da24d28b1e
bgercken <bgercken@comcast.net>
2018-11-09 21:02:12 -0500

    Updates

--- precache-manifest.36d8ac825cde542143525d711defe32a.js
+++ precache-manifest.36d8ac825cde542143525d711defe32a.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-contact-manager/static/js/runtime~main.ac3c48b9.js"
   },
   {
-    "revision": "8748e689f0beca29aa4a",
-    "url": "/react-contact-manager/static/js/main.8748e689.chunk.js"
+    "revision": "fa903ea25880f76349dd",
+    "url": "/react-contact-manager/static/js/main.fa903ea2.chunk.js"
   },
   {
     "revision": "9095a9b9ebacb3b6ed54",
     "url": "/react-contact-manager/static/js/1.9095a9b9.chunk.js"
   },
   {
-    "revision": "8748e689f0beca29aa4a",
+    "revision": "fa903ea25880f76349dd",
     "url": "/react-contact-manager/static/css/main.5bf5a46e.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-contact-manager/static/css/1.bbc2ebcc.chunk.css"
   },
   {
-    "revision": "f3f286bbfc8b7230a02bf78ec00c4540",
+    "revision": "76d9402865978b53607c0b421f50e7bb",
     "url": "/react-contact-manager/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-contact-manager/precache-manifest.d69a6fce2877d7e797ade092987e0cd9.js"
+  "/react-contact-manager/precache-manifest.36d8ac825cde542143525d711defe32a.js"
 );
 
 workbox.clientsClaim();
