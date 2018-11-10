f15694b65a724ba6d01a400bb4615d8ff2342cd5
bgercken <bgercken@comcast.net>
2018-11-09 19:02:38 -0500

    Updates

---
+++ precache-manifest.2ffa10fb8c627bf79c72493b45d5e0b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac3c48b9cdbff86df9e4",
+    "url": "/react-contact-manager/static/js/runtime~main.ac3c48b9.js"
+  },
+  {
+    "revision": "1a8c05c3680a1a234d6f",
+    "url": "/react-contact-manager/static/js/main.1a8c05c3.chunk.js"
+  },
+  {
+    "revision": "9f3589ce5950479babb1",
+    "url": "/react-contact-manager/static/js/1.9f3589ce.chunk.js"
+  },
+  {
+    "revision": "1a8c05c3680a1a234d6f",
+    "url": "/react-contact-manager/static/css/main.2bf4efaf.chunk.css"
+  },
+  {
+    "revision": "9f3589ce5950479babb1",
+    "url": "/react-contact-manager/static/css/1.03ba1087.chunk.css"
+  },
+  {
+    "revision": "af6a86df664452a7b6a527d9c9c36248",
+    "url": "/react-contact-manager/index.html"
+  }
+];
